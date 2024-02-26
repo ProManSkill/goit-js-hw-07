0aa664268da8d8fb50449cf3657a825b57a81cc3
Volodymyr Bosenko <mrvovantheone@gmail.com>
2024-02-26 20:18:48 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form")
+form.addEventListener("submit", handleSubmit )
+function handleSubmit(event) {
+    event.preventDefault();
+    const elements = event.currentTarget.elements
+    if (elements.email.value.trim() === "" || elements.password.value.trim() === "") {
+        alert('All form fields must be filled in');
+    } else {
+        const info = {
+            email: elements.email.value.trim(),
+            password: elements.password.value.trim(),
 
+        }
+        console.log(info)
+    }
+    event.currentTarget.reset()
+    
+}
